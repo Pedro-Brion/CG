b9e246ec0eecff7d1cef0bbb734322e4aabbb2de
Pedro Brion <pedro.brion@gmail.com>
2022-10-16 23:51:33 -0300

    T1 concluído
    Co-authored-by: Gabriel Duque Schiffner <gabrielduqueschiffner@users.noreply.github.com>

--- T1/main.js
+++ T1/main.js
@@ -1,17 +1,24 @@
 import * as THREE from "three";
 import MainScene from "./scene.js";
 
-import { GLTFLoader } from "../build/jsm/loaders/GLTFLoader.js";
 import {
   initRenderer,
-  initDefaultBasicLight,
   onWindowResize,
+  createLightSphere,
+  initDefaultBasicLight,
 } from "../libs/util/util.js";
+import { TrackballControls } from "../build/jsm/controls/TrackballControls.js";
+import { arraySlice } from "three/src/animation/animationutils.js";
 
 let scene, renderer, light; // Initial variables
 renderer = initRenderer(); // Init a basic renderer
 scene = new MainScene(); // Create main scene
-const camera= scene.camera.camera;
+
+const camera= scene.camera.cameraOrto;
+
+let cubes = [];
+
+//Light
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 
 // Listen window size changes
@@ -23,74 +30,47 @@ window.addEventListener(
   false
 );
 
-//
-
-// const modelName = './assets/armor.glb'
-
-// var loader = new GLTFLoader();
-// loader.load(
-//   modelName,
-//   function (gltf) {
-//     var obj = gltf.scene;
-//     obj.traverse(function (child) {
-//       if (child) {
-//         child.castShadow = true;
-//       }
-//     });
-//     obj.traverse(function (node) {
-//       if (node.material) node.material.side = THREE.DoubleSide;
-//     });
-
-//     // Only fix the position of the centered object
-//     // The man around will have a different geometric transformation
-
-//     obj = normalizeAndRescale(obj, 2);
-//     obj = fixPosition(obj);
-   
-//     scene.add(obj);
-
-//     // Create animationMixer and push it in the array of mixers
-//     var mixerLocal = new THREE.AnimationMixer(obj);
-//     mixerLocal.clipAction(gltf.animations[0]).play();
-//     mixer.push(mixerLocal);
-//   },
-//   onProgress,
-//   onError
-// );
-
-// function normalizeAndRescale(obj, newScale)
-// {
-//   var scale = getMaxSize(obj); // Available in 'utils.js'
-//   obj.scale.set(newScale * (1.0/scale),
-//                 newScale * (1.0/scale),
-//                 newScale * (1.0/scale));
-//   return obj;
-// }
-
-// function fixPosition(obj)
-// {
-//   // Fix position of the object over the ground plane
-//   var box = new THREE.Box3().setFromObject( obj );
-//   if(box.min.y > 0)
-//     obj.translateY(-box.min.y);
-//   else
-//     obj.translateY(-1*box.min.y);
-//   return obj;
-// }
-
-// function onError() { };
-
-// function onProgress ( xhr, model ) {
-//     if ( xhr.lengthComputable ) {
-//       var percentComplete = xhr.loaded / xhr.total * 100;
-//     }
-// }
-
-scene.initialize();
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 render();
 function render() {
-  scene.update();
   requestAnimationFrame(render);
-  renderer.render(scene, camera); // Render scene
+  const actualCamera = scene.getCamera();
+  // console.log(actualCamera)
+  
+	raycaster.setFromCamera( pointer, actualCamera );
+
+  renderer.render(scene, actualCamera); // Render scene  
+  scene.update();
 }
+
+function onPointerMove( event ) {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+function onClick (event){  
+  const intersects = raycaster.intersectObjects( scene.obstaclesObj );
+
+  if(cubes.map(cube=>cube.obj).includes(intersects[0].object)){
+    let index = cubes.map(cube=>cube.obj)
+      .indexOf(intersects[0].object);
+    
+    cubes[index].clicked = !cubes[index].clicked;
+    intersects[0].object.material.color.set( cubes[index].clicked ? 'rgb(150,95,100)' : 'rgb(150,150,30)' ) 
+  }
+  else{
+    cubes.push({
+      obj : intersects[0].object,
+      clicked : true
+    })
+    intersects[0].object.material.color.set( 'rgb(150,95,100)' ) 
+  }
+  console.log(cubes.map(cube=>cube.clicked))    
+}
+
+window.addEventListener( 'pointermove', onPointerMove );
+window.addEventListener( 'pointerdown', onClick );
+
+window.requestAnimationFrame(render);
